e584c62c644d825ea97430c110217234e49ecf2f
CameronIFrazier <taterfrazier1@gmail.com>
2024-04-21 19:59:35 -0700

    wip

--- transitiontable.py
+++ transitiontable.py
@@ -1,5 +1,11 @@
 state_transitiontable_table = {
-    'q0': {'-': 'q7'},
+    'q0': {'1':'deci accepting state', '2':'deci accepting state', '3':'deci accepting state', '4':'deci accepting state', '5':'deci accepting state',
+           '6':'deci accepting state', '7':'deci accepting state', '8':'deci accepting state', '9':'deci accepting state'},
+    'q19':{'0':'deci accepting state','1':'deci accepting state', '2':'deci accepting state', '3':'deci accepting state', '4':'deci accepting state', '5':'deci accepting state',
+        '6':'deci accepting state', '7':'deci accepting state', '8':'deci accepting state', '9':'deci accepting state'},
+    'deci accepting state': {'0':'deci accepting state','1':'deci accepting state', '2':'deci accepting state', '3':'deci accepting state', '4':'deci accepting state', '5':'deci accepting state',
+        '6':'deci accepting state', '7':'deci accepting state', '8':'deci accepting state', '9':'deci accepting state', '8':'deci accepting state','9':'deci accepting state', "_": 'q19'},
+
 
     'q8': {'B':'q13', 'b': 'q13', "_": 'rejected'},
     'q11': {'0':'binary accepting state', '1':'binary accepting state'},  
@@ -36,10 +42,11 @@ state_transitiontable_table = {
     'octa accepting state': {'0':'octa accepting state', '1':'octa accepting state', '2':'octa accepting state', '3':'octa accepting state', '4':'octa accepting state',
             '5':'octa accepting state', '6':'octa accepting state', '7':'octa accepting state', '_':'q23'}
 
+
 }
 
 temp = 'q0'
-ui = input("Please enter a string")
+ui = input("Please enter a string with no spaces: ")
 
 for index, char in enumerate(ui):
     if char == '0' and index + 1 < len(ui) and (ui[index + 1] == 'b' or ui[index + 1] == 'B'):
@@ -54,12 +61,16 @@ for index, char in enumerate(ui):
         temp = 'q15'
         print("You are now in " + temp)
         continue
+    try:
+        temp = state_transitiontable_table[temp][char]
+        print("You are now in " + str(temp))
+    except KeyError:
+        print("Invalid Literal. You entered a character that isn't acceptable. Keep in mind there can be no spaces.")
 
-    temp = state_transitiontable_table[temp][char]
-    print("you are now in " + str(temp))
-
-if temp =='hexi accpeting state' or temp == 'octa accepting state' or temp == 'binary accepting state':
+if temp =='hexi accpeting state' or temp == 'octa accepting state' or temp == 'binary accepting state' or temp == 'deci accepting state':
     print("Valid Literal")
+else:
+    print("Invalid Literal")
         
        
 '''need to put try catch around trans table in case the string contains something not in the dictionary'''
